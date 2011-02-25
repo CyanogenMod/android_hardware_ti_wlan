f8bab0c0f45b0e7744c8fc5fc9da1c46455f19c1
Vishal Mahaveer <vishalm@ti.com>
2011-02-25 15:57:49 -0700

    WLAN - wl1283_softAP: allowing driver commands
    Driver commands check was removed when moving to new driver version.
    Undoing the temporary change
    
    Change-Id: Ib6e5ee9960c987b55b6ae677d403626d8d260521

--- wl1283_softAP/platforms/os/linux/src/WlanDrvWext.c
+++ wl1283_softAP/platforms/os/linux/src/WlanDrvWext.c
@@ -172,13 +172,11 @@ int wlanDrvWext_Handler (struct net_device *dev,
    void             *copy_to_buf=NULL, *param3=NULL;
    ECmdType         CmdType = CMD_WEXT_CMD_E;
 
-#if 0
    /* abort if the IOCTL is disabled */
    if (!wlanDrvIf_IsIoctlEnabled(drv, info->cmd))
    {
 	   return TI_NOK;
    }
-#endif
 
    os_memoryZero(drv, &my_command, sizeof(ti_private_cmd_t));
    os_memoryZero(drv, &mlme,       sizeof(struct iw_mlme));
@@ -196,13 +194,11 @@ int wlanDrvWext_Handler (struct net_device *dev,
 		 return TI_NOK;
 	   }
 
-#if 0
        /* abort if the private-command is disabled */
        if (!wlanDrvIf_IsCmdEnabled(drv, my_command.cmd))
        {
 		return TI_NOK;
        }
-#endif
 
 	   if (IS_PARAM_FOR_MODULE(my_command.cmd, DRIVER_MODULE_PARAM))
        {
