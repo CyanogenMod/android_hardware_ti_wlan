f19e2c6a5e8e940dda62a60db7274e65cfa5bc7b
Vishal Mahaveer <vishalm@ti.com>
2011-05-24 15:26:56 -0600

    WLAN - wl1283: Add DPLL cascading blockers
    Adding DPLL cascade blockers when Wi-Fi in use.
    
    This will prevent system from entering DPLL cascading state when Wi-Fi is in use.
    Changes applied in same area where OPP constraints are added.
    
    Change-Id: I123af9203b66ce53b3fc588c1a02d6e5906c7a42

--- wl1283/external_drivers/Linux/standard_sdio/SdioDrv.c
+++ wl1283/external_drivers/Linux/standard_sdio/SdioDrv.c
@@ -49,6 +49,7 @@
 #include <linux/mmc/sdio_ids.h>
 #include <linux/mmc/sdio_func.h>
 #include <plat/omap_device.h>
+#include <mach/omap4-common.h>
 
 #include "SdioDrvDbg.h"
 #include "SdioDrv.h"
@@ -117,6 +118,9 @@ void sdioDrv_cancel_inact_timer(void)
 static void sdioDrv_opp_setup(struct work_struct *work)
 {
 	omap_pm_set_min_mpu_freq(&dummy_cpufreq_dev.dev, -1);
+
+	/* Release DPLL cascading blockers when we are done with Wi-Fi */
+	dpll_cascading_blocker_release(&dummy_cpufreq_dev.dev);
 }
 
 void sdioDrv_ClaimHost(unsigned int uFunc)
@@ -130,7 +134,11 @@ void sdioDrv_ClaimHost(unsigned int uFunc)
 
 	g_drv.sdio_host_claim_ref = 1;
 
+	/* Call to black DPLL when Wi-Fi is in use */
+	dpll_cascading_blocker_hold(&dummy_cpufreq_dev.dev);
+
 	omap_pm_set_min_mpu_freq(&dummy_cpufreq_dev.dev, OMAP_MPU_OPP_1GHZ);
+
 	sdio_claim_host(tiwlan_func[uFunc]);
 }
 
@@ -435,6 +443,7 @@ static void tiwlan_sdio_remove(struct sdio_func *func)
 
 	/* Release all constraints at exit */
 	omap_pm_set_min_mpu_freq(&dummy_cpufreq_dev.dev, -1);
+	dpll_cascading_blocker_release(&dummy_cpufreq_dev.dev);
 
 	tiwlan_func[SDIO_WLAN_FUNC] = NULL;
 	tiwlan_func[SDIO_CTRL_FUNC] = NULL;
