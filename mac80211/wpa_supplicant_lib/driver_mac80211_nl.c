36fc0860a1d106a237ba217b951c25f7e152a8ea
Vishal Mahaveer <a0271468@ti.com>
2011-07-29 14:29:29 -0600

    WLAN - mac80211: add mode commands to supplicant lib
    Adding more DRIVER commands support to wpa_supplicant_lib using nl
    
    Change-Id: I16d7670f25e246fa8159a5d86ea96b532d857b7b
    Signed-off-by: Vishal Mahaveer <a0271468@ti.com>

--- mac80211/wpa_supplicant_lib/driver_mac80211_nl.c
+++ mac80211/wpa_supplicant_lib/driver_mac80211_nl.c
@@ -27,6 +27,78 @@
 #include "driver_nl80211.h"
 
 #define WPA_EVENT_DRIVER_STATE          "CTRL-EVENT-DRIVER-STATE "
+#define DRV_NUMBER_SEQUENTIAL_ERRORS     4
+
+#define WPA_PS_ENABLED   0
+#define WPA_PS_DISABLED  1
+
+#define BLUETOOTH_COEXISTENCE_MODE_ENABLED   0
+#define BLUETOOTH_COEXISTENCE_MODE_DISABLED  1
+#define BLUETOOTH_COEXISTENCE_MODE_SENSE     2
+
+
+static int g_drv_errors = 0;
+static int g_power_mode = 0;
+
+int send_and_recv_msgs(struct wpa_driver_nl80211_data *drv, struct nl_msg *msg,
+                       int (*valid_handler)(struct nl_msg *, void *),
+                       void *valid_data);
+
+static void wpa_driver_send_hang_msg(struct wpa_driver_nl80211_data *drv)
+{
+	g_drv_errors++;
+	if (g_drv_errors > DRV_NUMBER_SEQUENTIAL_ERRORS) {
+		g_drv_errors = 0;
+		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "HANGED");
+	}
+}
+
+static int wpa_driver_toggle_btcoex_state(char state)
+{
+	int ret;
+	int fd = open("/sys/devices/platform/wl1271/bt_coex_state", O_RDWR, 0);
+	if (fd == -1)
+		return -1;
+
+	ret = write(fd, &state, sizeof(state));
+	close(fd);
+
+	wpa_printf(MSG_DEBUG, "%s:  set btcoex state to '%c' result = %d", __func__,
+		   state, ret);
+	return ret;
+}
+
+static int wpa_driver_set_power_save(void *priv, int state)
+{
+	struct i802_bss *bss = priv;
+	struct wpa_driver_nl80211_data *drv = bss->drv;
+	struct nl_msg *msg;
+	int ret = -1;
+	enum nl80211_ps_state ps_state;
+
+	msg = nlmsg_alloc();
+	if (!msg)
+		return -1;
+
+	genlmsg_put(msg, 0, 0, genl_family_get_id(drv->nl80211), 0, 0,
+		    NL80211_CMD_SET_POWER_SAVE, 0);
+
+	if (state == WPA_PS_ENABLED)
+		ps_state = NL80211_PS_ENABLED;
+	else
+		ps_state = NL80211_PS_DISABLED;
+
+	NLA_PUT_U32(msg, NL80211_ATTR_IFINDEX, drv->ifindex);
+	NLA_PUT_U32(msg, NL80211_ATTR_PS_STATE, ps_state);
+
+	ret = send_and_recv_msgs(drv, msg, NULL, NULL);
+	msg = NULL;
+	if (ret < 0)
+		wpa_printf(MSG_ERROR, "nl80211: Set power mode fail: %d", ret);
+nla_put_failure:
+	nlmsg_free(msg);
+	return ret;
+}
 
 int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 				  size_t buf_len )
@@ -36,14 +108,36 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 	struct ifreq ifr;
 	int ret = 0;
 
-	wpa_printf(MSG_INFO, "%s: %s", __func__, cmd);
-
 	if (os_strcasecmp(cmd, "STOP") == 0) {
 		linux_set_iface_flags(drv->ioctl_sock, bss->ifname, 0);
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "STOPPED");
 	} else if (os_strcasecmp(cmd, "START") == 0) {
 		linux_set_iface_flags(drv->ioctl_sock, bss->ifname, 1);
 		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "STARTED");
+	} else if (os_strcasecmp(cmd, "RELOAD") == 0) {
+		wpa_msg(drv->ctx, MSG_INFO, WPA_EVENT_DRIVER_STATE "HANGED");
+	} else if (os_strncasecmp(cmd, "POWERMODE ", 10) == 0) {
+		int mode;
+		mode = atoi(cmd + 10);
+		ret = wpa_driver_set_power_save(priv, mode);
+		if (ret < 0) {
+			wpa_driver_send_hang_msg(drv);
+		} else {
+			g_power_mode = mode;
+			g_drv_errors = 0;
+		}
+	} else if (os_strncasecmp(cmd, "GETPOWER", 8) == 0) {
+		ret = os_snprintf(buf, buf_len, "POWERMODE = %d\n", g_power_mode);
+	} else if (os_strncasecmp(cmd, "BTCOEXMODE ", 11) == 0) {
+		int mode = atoi(cmd + 11);
+		if (mode == BLUETOOTH_COEXISTENCE_MODE_DISABLED) { /* disable BT-coex */
+			ret = wpa_driver_toggle_btcoex_state('0');
+		} else if (mode == BLUETOOTH_COEXISTENCE_MODE_SENSE) { /* enable BT-coex */
+			ret = wpa_driver_toggle_btcoex_state('1');
+		} else {
+			wpa_printf(MSG_DEBUG, "invalid btcoex mode: %d", mode);
+			ret = -1;
+		}
 	} else if (os_strcasecmp(cmd, "MACADDR") == 0) {
 		u8 macaddr[ETH_ALEN] = {};
 
