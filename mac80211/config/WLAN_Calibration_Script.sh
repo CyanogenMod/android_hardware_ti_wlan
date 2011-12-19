e50bafeca35c96c928fc1a3e87294859818fc2b2
Vishal Mahaveer <vishalm@ti.com>
2011-12-19 17:31:00 -0600

    WLAN - Adjust WLAN calibration script to new calibrator
    Calibrator was updated and the procedure needs to be updated accordingly.
    
    Change-Id: I268eb6c984cf5873b2f3fbaebaa6301ea0bf76c4
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- mac80211/config/WLAN_Calibration_Script.sh
+++ mac80211/config/WLAN_Calibration_Script.sh
@@ -13,38 +13,20 @@ case "$WIFION" in
 esac
 
 TARGET_FW_DIR=/system/etc/firmware/ti-connectivity
-TARGET_FW_FILE=$TARGET_FW_DIR/wl1271-nvs.bin
-TARGET_NVS_FILE=/system/etc/wifi/TQS_D_1.7.ini
+TARGET_NVS_FILE=$TARGET_FW_DIR/wl1271-nvs.bin
+TARGET_INI_FILE=/system/etc/wifi/TQS_D_1.7.ini
+WL12xx_MODULE=/system/lib/modules/wl12xx_sdio.ko
 
 # Remount system partition as rw
 mount -o remount rw /system
 
-cd $TARGET_FW_DIR
+# Remove old NVS file
+rm /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 
-# Create reference NVS file
-calibrator set ref_nvs $TARGET_NVS_FILE
-mv ./new-nvs.bin $TARGET_FW_FILE
-
-# Load driver
-/system/bin/insmod /system/lib/modules/wl12xx_sdio.ko
-
-# Calibrate...
-calibrator wlan0 plt power_mode on
-calibrator wlan0 plt tune_channel 0 7
-calibrator wlan0 plt tx_bip 1 1 1 1 1 1 1 1
-calibrator wlan0 plt power_mode off
-
-# Update NVS with random mac address (Don't give second argument of MAC adress)
-calibrator set nvs_mac ./new-nvs.bin
-#calibrator set nvs_mac ./new-nvs.bin 08:00:28:12:12:12
-
-/system/bin/rmmod wl12xx_sdio
-/system/bin/rmmod wl12xx
-
-mv ./new-nvs.bin $TARGET_FW_FILE
-
-/system/bin/insmod /system/lib/modules/wl12xx.ko
-
+# Actual calibration...
+# calibrator plt autocalibrate <dev> <module path> <ini file1> <nvs file> <mac addr>
+# Leaving mac address field empty for random mac
+calibrator plt autocalibrate wlan0 $WL12xx_MODULE $TARGET_INI_FILE $TARGET_NVS_FILE
 
 echo " ******************************"
 echo " * Finished Wi-Fi calibration *"
