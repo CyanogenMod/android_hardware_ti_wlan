31f2c1b14ed95cf83f7ab794021c98825e33a07a
Vishal Mahaveer <vishalm@ti.com>
2012-08-10 09:31:59 -0500

    wlan: mac80211: config: add check for new wpa_supplicant tree
    Add check for new TI wpa_supplicant tree added to JB
    
    Change-Id: I7525f8a6792b2a9b909d6365d2ef13b502f417a6
    Signed-off-by: Vishal Mahaveer <vishalm@ti.com>

--- mac80211/config/Android.mk
+++ mac80211/config/Android.mk
@@ -76,10 +76,14 @@ WIFI_DRIVER_SOCKET_IFACE := wlan0
 ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_8_X)
   include external/wpa_supplicant_8/wpa_supplicant/wpa_supplicant_conf.mk
 else
+ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_8_X_TI)
+  include external/wpa_supplicant_8_ti/wpa_supplicant/wpa_supplicant_conf.mk
+else
 ifeq ($(strip $(WPA_SUPPLICANT_VERSION)),VER_0_6_X)
   include external/wpa_supplicant_6/wpa_supplicant/wpa_supplicant_conf.mk
 else
   include external/wpa_supplicant/wpa_supplicant_conf.mk
+endif
 endif
 endif
 
