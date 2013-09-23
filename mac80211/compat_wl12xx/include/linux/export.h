3f1de13dfdb98c0503808349a8826e6bdda531c7
Thomas Wendt <thoemy@gmx.net>
2013-09-24 00:10:42 +0200

    WLAN - wl12xx: Allow building against kernel version 3.1
    Change-Id: I1d71aae536487bdc4cb6bd9455f33be1790f961d

--- mac80211/compat_wl12xx/include/linux/export.h
+++ mac80211/compat_wl12xx/include/linux/export.h
@@ -3,10 +3,10 @@
 
 #include <linux/version.h>
 
-#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,2,0))
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0))
 #include_next <linux/export.h>
 #else
 #include <linux/module.h>
-#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,2,0)) */
+#endif /* (LINUX_VERSION_CODE >= KERNEL_VERSION(3,1,0)) */
 
 #endif	/* _COMPAT_LINUX_EXPORT_H */
