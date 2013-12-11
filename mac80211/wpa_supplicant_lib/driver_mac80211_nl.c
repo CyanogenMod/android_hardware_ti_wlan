f556ddffb045e899bdd0ea44492e66d2615ba68e
Arik Nemtsov <arik@wizery.com>
2013-12-11 13:56:06 +0200

    Android: driver_nl80211: implement NoA/p2p_ps/set_wps_ie stubs
    Most return -1, as if the functions were not implemented. This allows
    us to compile with ANDROID_P2P. set_wps_ie returns 0 to allow AP
    initialization to work.
    
    Signed-off-by: Arik Nemtsov <arik@wizery.com>
    Change-Id: I9a454782524270e3db5af45b0aadd051c6fa41a0

--- mac80211/wpa_supplicant_lib/driver_mac80211_nl.c
+++ mac80211/wpa_supplicant_lib/driver_mac80211_nl.c
@@ -97,3 +97,25 @@ int wpa_driver_nl80211_driver_cmd(void *priv, char *cmd, char *buf,
 	}
 	return ret;
 }
+
+int wpa_driver_set_p2p_noa(void *priv, u8 count, int start, int duration)
+{
+	return -1;
+}
+
+int wpa_driver_get_p2p_noa(void *priv, u8 *buf, size_t len)
+{
+	return -1;
+}
+
+int wpa_driver_set_p2p_ps(void *priv, int legacy_ps, int opp_ps, int ctwindow)
+{
+	return -1;
+}
+
+int wpa_driver_set_ap_wps_p2p_ie(void *priv, const struct wpabuf *beacon,
+				 const struct wpabuf *proberesp,
+				 const struct wpabuf *assocresp)
+{
+	return 0;
+}
