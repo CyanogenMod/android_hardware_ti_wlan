9ae3009c46f2d7a45cd85ad0f59b049326b4c5ca
Thomas Wendt <thoemy@gmx.net>
2014-01-21 01:11:25 +0100

    wl127x/wl128x: add firmware from linux-firmware.git
        wl127x/wl128x: update firmwares
    
        Update the wl127x and wl128x firmwares to *.3.10.0.133 for single-role
        and *.5.7.0.42 for multi-role.
    
        Signed-off-by: Luciano Coelho <coelho@ti.com>
    
        wl127x/wl128x: add new firmware series as version 5
    
        There are some changes in the newer firmware versions that are not
        backwards compatible with older versions of the driver.  Starting on
        Linux 3.6, we will need these new versions.
    
        The new versions are X.3.10.2.115 for single-role and X.5.7.0.27 for
        multi-role.
    
        Signed-off-by: Luciano Coelho <coelho@ti.com>
    
    Change-Id: I0c502ada6f2ae841e110e7f2c92ba08de8162204

--- mac80211/firmware/128x/Android.mk
+++ mac80211/firmware/128x/Android.mk
@@ -1,8 +1,10 @@
+ifeq ($(BOARD_WLAN_CHIPSET),wl128x)
+
 local_target_dir := $(TARGET_OUT)/etc/firmware/ti-connectivity
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-4-sr.bin
+LOCAL_MODULE := wl128x-fw-5-sr.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -10,7 +12,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-4-mr.bin
+LOCAL_MODULE := wl128x-fw-5-mr.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -18,7 +20,7 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl128x-fw-4-plt.bin
+LOCAL_MODULE := wl128x-fw-5-plt.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(local_target_dir)
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
@@ -26,9 +28,11 @@ LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := wl1271-nvs_128x.bin
+LOCAL_MODULE := wl1271-nvs.bin
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(local_target_dir)
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
+LOCAL_SRC_FILES := wl1271-nvs_128x.bin
 LOCAL_MODULE_CLASS := FIRMWARE
 include $(BUILD_PREBUILT)
+
+endif
